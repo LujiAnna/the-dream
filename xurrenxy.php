6466158136531cf020bc87f6cba7c28781e04678
Anna Ikoki <ikokianna@gmail.com>
2021-07-28 11:30:52 +0200

    Remove html and body tags in php file

--- xurrenxy.php
+++ xurrenxy.php
@@ -1,14 +1,10 @@
-<html>
 
-<body>
-  <?php
-  # post method for sensitive data
-  $amount = $_GET['amount'];
-  $euro = $amount * 0.16;
-  $answer = "<br/>$amount Brazilian Real equals $euro Euro<br/>";
-  echo $answer;
+<?php
+# post method for sensitive data
+$amount = $_GET['amount'];
+$euro = $amount * 0.16;
+$answer = "<br/>$amount Brazilian Real equals $euro Euro<br/>";
+// echo $answer; // makes page download this form instead of showing on browser
+echo $answer;
 
-  ?>
-</body>
-
-</html>
+?>
