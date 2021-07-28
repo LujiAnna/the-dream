4c4216fd8572e4b201ca602ec44cbc3713d7ea71
Anna Ikoki <ikokianna@gmail.com>
2021-07-28 12:28:57 +0200

    Change method from get to post

--- xurrenxy.php
+++ xurrenxy.php
@@ -1,10 +1,18 @@
 
 <?php
 # post method for sensitive data
-$amount = $_GET['amount'];
-$euro = $amount * 0.16;
-$answer = "<br/>$amount Brazilian Real equals $euro Euro<br/>";
-// echo $answer; // makes page download this form instead of showing on browser
-echo $answer;
+$amount = $_POST['amount'];
+# Check if amount field is 0, empty, or not set at all'
+# Read more: https://www.php.net/manual/en/function.empty.php
+if (empty($amount)) {
+  # If the field is empty, display a message to the user
+  echo " <br/> Please fill in the amount";
+  # Process the form data if the input field is not empty
+} else {
+  $euro = $amount * 0.16;
+  $answer = "<br/>$amount Brazilian Real equals $euro Euro<br/>";
+  // echo $answer; // makes page download this form instead of showing on browser
+  echo $answer;
+}
 
 ?>
